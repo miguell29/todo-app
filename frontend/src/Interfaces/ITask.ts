e91c9ce3517895231afc5331cc9459620796df39
Miguel Juarez <mfjuaarez@gmail.com>
2025-06-05 11:29:53 -0300

    Se creo creo la vista de tareas y se corrigio un error de tipeo en itask.ts

--- frontend/src/Interfaces/ITask.ts
+++ frontend/src/Interfaces/ITask.ts
@@ -3,5 +3,5 @@ export interface ITask {
     title:string,
     description: string,
     completed: boolean,
-    createdAd: Date
+    createdAt: Date
 }
--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -1,20 +1,26 @@
 import { useState, useEffect } from "react";
-import { ITask } from "../Interfaces/ITask";
-
+import type { ITask } from "../Interfaces/ITask";
+import { Col, Row, Container, Table, Button } from "reactstrap";
+import { Link } from "react-router-dom";
 
 
 export const TaskList = () => {
 
-    const [Task, setTask] = useState<ITask[]>([]);
+    const [Tasks, setTasks] = useState<ITask[]>([]);
 
     const getTasks = async() => {
         const res = await fetch(`${import.meta.env.VITE_API_URL}`);
         if (res.ok) {
             const data = await res.json();
-            setTask(data);
+            setTasks(data);
             console.log(data);
             
         }
+    }
+
+    const deleteTask = (id: string) => {
+        console.log(id);
+        
     }
 
     useEffect(() => {
@@ -24,8 +30,37 @@ export const TaskList = () => {
 
     return (
         <>
-            <h1>lista</h1>
-            <p>{Task[0].title}</p>
+            <Container className="mt-5">
+                <Row>
+                    <Col sm={{size:8, offset:2}}>
+                        <h4>Lista de Tareas</h4>
+                        <hr />
+                        <Link className="btn btn-success mb-3" to="/TaskForm">Agregar Tarea</Link>
+
+                        <Table bordered>
+                            <thead>
+                                <tr>
+                                    <th>Titulo</th>
+                                    <th>Fecha De creacion</th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                                    </thead>
+                                <tbody>
+                                    {Tasks.map(task => (
+                                        <tr key={task.id}>
+                                            <td>{task.title}</td>
+                                            <td>{new Date(task.createdAt).toUTCString()}</td>
+                                            <td><Link className="btn btn-outline-success mb-3" to={`/TaskItem/${task.id}`}>👀 detalles</Link></td>
+                                            <td><Link className="btn btn-outline-primary mb-3" to={`/TaskForm/${task.id}`}>Editar</Link></td>
+                                            <td><Button className="btn btn-outline-danger" onClick={() => {deleteTask(task.id)}}>Eliminar</Button></td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
